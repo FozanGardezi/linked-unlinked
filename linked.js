3a14ee3779a98c7bb42659b116bb3ed40a35d5b5
FozanGardezi <fozangardezi@gmail.com>
2021-03-13 20:47:48 +0500

    Create functionality to find unlinked users

--- linked.js
+++ linked.js
@@ -1,1 +1,19 @@
-// find all the users that have not been linked
+import {allUsers, linked} fom "users"
+
+const findUnlinkedUsers = () => {
+  var linkedUsersHash = {}
+  var unLinkedUsers = []
+
+  for (let i = 0 ; i < linked.length; i++) {
+   linkedUsersHash[linked[i].id] =  linked[i].id
+  }
+
+  for (let i = 0; i < allUsers.length; i++) {
+    if (linkedUsersHash[allUsers[i].id] === undefined) {
+     unLinkedUsers.push(allUsers[i])
+    }
+  }
+  return unLinkedUsers
+}
+
+findUnlinkedUsers();
